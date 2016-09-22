1ff37a8d2a0cb07260ffe59d72ca1387d81f99ec
jepetty <jessica.petty@colorado.edu>
2016-09-22 17:59:00 -0600

    refactor for ease of part 2

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -2,26 +2,27 @@ import random
 
 
 def main():
-	generate_X()
+	print(generate_X(6000))
 
-def generate_X():
+def generate_X(n):
 	count = 0
 	perm = [6, 7, 2, 5, 1, 4,3 ]
-	for i in range(0,6000):
-		xarray = []
-		xdict = {}
-		for j in range(1,8):
-			x = random.randint(0,1000)/1000
-			xarray.append(x)
-			xdict[x] = j
-		xarray.sort()
-		jarray = []
-		for k in range(0,7):
-			jarray.append(xdict[xarray[6-k]])
-		if jarray == perm:
-			count = count + 1
-	print(count)
+	for i in range(0,n):
+		sim = generate_random(7)
+	return count
 
+def generate_random(count):
+	xarray[]
+	xdict{}
+	for j in range(1, count+1):
+		x = random.randint(0,1000)/1000
+		xarray.append(x)
+		xdict[x] = j
+	xarray.sort()
+	jarray = []
+	for k in range(0,count):
+		jarray.append(xdict[xarray[count-1-k]])
+	return jarray
 
 if __name__ == "__main__":
 	main()
