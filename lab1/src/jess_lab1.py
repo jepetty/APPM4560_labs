0f7cff55cfb7cf97ef4facd0ac6d4da5db79d151
jepetty <jessica.petty@colorado.edu>
2016-09-25 11:21:36 -0600

    finish part 2 coding for the lab, minor changes

--- lab1/src/jess_lab1.py
+++ lab1/src/jess_lab1.py
@@ -1,16 +1,17 @@
 import random
+import math
 
 def main():
-	print(generate_X(6000))
+	#print(generate_X(6000))
 	average9 = 0
 	average21 = 0
 	average36 = 0
 	average69 = 0
 	for i in range(0,10000):
-		average9 = average9 + find_first_position(9)
-		average21 = average21 + find_first_position(21)
-		average36 = average36 + find_first_position(36)
-		average69 = average69 + find_first_position(69)
+		average9 = average9 + find_first_positionB(9)
+		average21 = average21 + find_first_positionB(21)
+		average36 = average36 + find_first_positionB(36)
+		average69 = average69 + find_first_positionB(69)
 	average9 = average9/10000
 	average21 = average21/10000
 	average36 = average36/10000
@@ -19,6 +20,8 @@ def main():
 	print("21 n: ",average21)
 	print("36 n: ",average36)
 	print("69 n: ",average69)
+
+	find_first_positionB(9)
 
 def generate_X(n):
 	count = 0
@@ -42,11 +45,28 @@ def generate_random(count):
 		jarray.append(xdict[xarray[count-1-k]])
 	return jarray
 
-def find_first_position(n):
+def find_first_positionA(n):
 	sim = generate_random(n)
 	for i in range(0, n):
 		if sim[i] == 1:
 			return i+1
+
+def find_first_positionB(n):
+	sim = generate_random(n)
+	found = False
+	count = 0
+	while (not found):
+		halfn = math.floor(n/2)
+		count = count + 1
+		if len(sim) <= 2:
+			return count
+		elif 1 in sim[0:halfn]:
+			sim = sim[0:halfn]
+			n = len(sim)
+		else:
+			sim = sim[halfn:]
+			n = len(sim)
+
 
 if __name__ == "__main__":
 	main()
