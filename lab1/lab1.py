6372144a938c69d541ed507f5d47f1f56966220c
jepetty <jessica.petty@colorado.edu>
2016-09-24 20:51:09 -0600

    pulling work from rhys

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,8 +1,24 @@
 import random
 
-
 def main():
 	print(generate_X(6000))
+	average9 = 0
+	average21 = 0
+	average36 = 0
+	average69 = 0
+	for i in range(0,10000):
+		average9 = average9 + find_first_position(9)
+		average21 = average21 + find_first_position(21)
+		average36 = average36 + find_first_position(36)
+		average69 = average69 + find_first_position(69)
+	average9 = average9/10000
+	average21 = average21/10000
+	average36 = average36/10000
+	average69 = average69/10000
+	print("9 n: ",average9)
+	print("21 n: ",average21)
+	print("36 n: ",average36)
+	print("69 n: ",average69)
 
 def generate_X(n):
 	count = 0
@@ -17,7 +33,7 @@ def generate_random(count):
 	xarray=[]
 	xdict={}
 	for j in range(1, count+1):
-		x = random.randint(0,1000)/1000
+		x = random.random()
 		xarray.append(x)
 		xdict[x] = j
 	xarray.sort()
@@ -25,6 +41,12 @@ def generate_random(count):
 	for k in range(0,count):
 		jarray.append(xdict[xarray[count-1-k]])
 	return jarray
+
+def find_first_position(n):
+	sim = generate_random(n)
+	for i in range(0, n):
+		if sim[i] == 1:
+			return i+1
 
 if __name__ == "__main__":
 	main()
