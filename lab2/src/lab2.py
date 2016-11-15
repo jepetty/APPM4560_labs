0e6ff824d12d054c934060d30cfe86979619683c
jepetty <jessica.petty@colorado.edu>
2016-11-14 23:13:27 -0700

    Fix labels in earlier plots, complete plotting for nhpp problem

--- lab2/src/lab2.py
+++ lab2/src/lab2.py
@@ -6,9 +6,21 @@ import matplotlib.pyplot as plt
 
 def main():
     simulate_hpp()
+    simulate_nhpp()
 
 def simulate_nhpp():
     t = 9
+    # max value of our lambda function occurs at x = 0, lambda = 26
+    C = 26
+    sims = [nhpp(C,t) for i in range (0, 10000)]
+
+    Ns = [len(sim) - 1 for sim in sims]
+
+    diagramNs = plt.hist(Ns, 10)
+    plt.title("Simulations of $N$ over $[0,9]$ for NHPP($\lambda(t)=t^2 - 10t + 26$)")
+    plt.xlabel("$N$")
+    plt.ylabel("Count of $N$")
+    plt.show(diagramNs)
 
 def simulate_hpp():
     intensity = 3
@@ -34,27 +46,27 @@ def simulate_hpp():
     Zs = [sim[-1] for sim in sims]
     
     diagramNs = plt.hist(Ns, 10)
-    plt.title("Simulations of N over [0,4] for HPP(3)")
-    plt.xlabel("N")
-    plt.ylabel("Count of N")
+    plt.title("Simulations of $N$ over $[0,4]$ for HPP(3)")
+    plt.xlabel("$N$")
+    plt.ylabel("Count of $N$")
     plt.show(diagramNs)
     
     diagramXs = plt.hist(Xs, 10)
-    plt.title("Simulations of T(N+1) - T(N) over [0,4] for HPP(3)")
-    plt.xlabel("T(N+1) - T(N)")
-    plt.ylabel("Count of T(N+1) - T(N)")
+    plt.title("Simulations of $T(N+1) - T(N)$ over $[0,4]$ for HPP(3)")
+    plt.xlabel("$T(N+1) - T(N)$")
+    plt.ylabel("Count of $T(N+1) - T(N)$")
     plt.show(diagramXs)
     
     diagramYs = plt.hist(Ys, 10)
-    plt.title("Simulations of T(N+1) - t over [0,4] for HPP(3)")
-    plt.xlabel("T(N+1) - t")
-    plt.ylabel("Count of T(N+1) - t")
+    plt.title("Simulations of $T(N+1) - t$ over $[0,4]$ for HPP(3)")
+    plt.xlabel("$T(N+1) - t$")
+    plt.ylabel("Count of $T(N+1) - t$")
     plt.show(diagramYs)
 
     diagramZs = plt.hist(Zs, 10)
-    plt.title("Simulations of T(N+1) over [0,4] for HPP(3)")
-    plt.xlabel("T(N+1)")
-    plt.ylabel("Count of T(N+1)")
+    plt.title("Simulations of $T(N+1)$ over $[0,4]$ for HPP(3)")
+    plt.xlabel("$T(N+1)$")
+    plt.ylabel("Count of $T(N+1)$")
     plt.show(diagramZs)
     
     print("Grr")
@@ -81,8 +93,8 @@ def nhpp(C, t):
         T1 = T[i - 1] - (math.log(U1)/C)
         T.append(T1)
         U2 = random.random()
-        lambda_func = T1^2 - 10*T1 + 26
-        if U2 > T1:
+        lambda_func = T1*T1 - 10*T1 + 26
+        if U2 > lambda_func:
             return T
     return T
 
