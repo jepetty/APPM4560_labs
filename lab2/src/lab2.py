b65fc540fafb6a5c284474daba6ae76169ec8821
Rhys Braginton Petteeeeeeetettte Olsen the Eleventeenth <rhys.olsen@colorado.edu>
2016-11-15 00:57:40 -0700

    Overlay distributions on histograms

--- lab2/src/lab2.py
+++ lab2/src/lab2.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 import random
 import math
+
+
+import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+
+from scipy.stats import expon
+from scipy.stats import gamma
 
 def main():
     intensity = 3
@@ -18,7 +24,6 @@ def main():
                 else:
                     f.write('\n')
     '''
-    print("Hmmm")
     Ns = [len(sim) - 1 for sim in sims]
     # T(N + 1) - T(N)
     Xs = [sim[-1] - sim[-2] for sim in sims]
@@ -26,22 +31,30 @@ def main():
     Ys = [sim[-1] - t for sim in sims]
     # T(N + 1)
     Zs = [sim[-1] for sim in sims]
+
+    NsSupport = np.linspace(0, max(Ns))
+    theoryNs  = plt.plot(NsSupport, gamma(4).pdf(NsSupport)) 
+    diagramNs = plt.hist(Ns, 15, normed=True)
+    plt.show()
     
-    diagramNs = plt.hist(Ns, 10)
-    plt.show(diagramNs)
-    
-    diagramXs = plt.hist(Xs, 10)
-    plt.show(diagramXs)
-    
-    diagramYs = plt.hist(Ys, 10)
-    plt.show(diagramYs)
+    # actualXs  = plt.hist(Xs, 10)
+    XsSupport = np.linspace(0, max(Xs))
+    theoryXs  = plt.plot(XsSupport, expon().pdf(XsSupport)) 
+    diagramXs = plt.hist(Xs, 15, normed=True)
+    plt.show()
+
+    YsSupport = np.linspace(0,int(max(Ys) + 1.0))
+    theoryYs  = plt.plot(YsSupport, expon().pdf(YsSupport)) 
+    diagramYs = plt.hist(Ys, 15, normed=True)
+    plt.show()
 
     diagramZs = plt.hist(Zs, 10)
-    plt.show(diagramZs)
-    
-    print("Grr")
+    plt.show()
+
+    '''
     while True:
         continue
+    '''
     
 def hpp(intensity, t):
     i = 0
